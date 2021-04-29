bd6b1ec2758887c7a5fe991e268dbeee34172736
Tauan Almeida <tauan_almeida@hotmail.com>
2021-04-29 15:14:19 -0300

    Adicionado component breadcrumb nas pages

---
+++ src/pages/CicloOperacional/Breadcrumb.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Link as RouterLink } from 'react-router-dom';
+import { Breadcrumbs, Grid, Link, Typography } from '@material-ui/core';
+import NavigateNextIcon from '@material-ui/icons/NavigateNext';
+
+const Breadcrumb = ({ ...rest }) => {
+  return (
+    <Grid container spacing={3} justify="space-between" {...rest}>
+      <Grid item>
+        <Breadcrumbs
+          separator={<NavigateNextIcon fontSize="small" />}
+          aria-label="breadcrumb"
+        >
+          <Link variant="body1" color="inherit" to="/" component={RouterLink}>
+            Dashboard
+          </Link>
+          <Typography variant="body1" color="textPrimary">
+            Ciclo operacional
+          </Typography>
+        </Breadcrumbs>
+      </Grid>
+    </Grid>
+  );
+};
+
+export default Breadcrumb;
