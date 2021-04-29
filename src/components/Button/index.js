769f7153fcccfd8329d24fe83d031a857bd3eb2e
Ramon <ramondavi@msn.com>
2021-04-29 17:53:50 -0300

    formatacao no component button

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,45 +1,6 @@
 import React from 'react';
-import { withStyles, makeStyles} from '@material-ui/core/styles';
-import Button from '@material-ui/core/Button';
-
-const StyleButton = withStyles({
-  root: {
-    boxShadow: 'none',
-    textTransform: 'none',
-    fontSize: 16,
-    padding: '6px 12px',
-    border: '1px solid',
-    lineHeight: 1.5,
-    backgroundColor: '#0068e9',
-    borderColor: '#0068e9',
-    width: '100%',
-    fontFamily: [
-      '-apple-system',
-      'BlinkMacSystemFont',
-      '"Segoe UI"',
-      'Roboto',
-      '"Helvetica Neue"',
-      'Arial',
-      'sans-serif',
-      '"Apple Color Emoji"',
-      '"Segoe UI Emoji"',
-      '"Segoe UI Symbol"',
-    ].join(','),
-    '&:hover': {
-      backgroundColor: '#0058c4',
-      borderColor: '#0058c4',
-      boxShadow: 'none',
-    },
-    '&:active': {
-      boxShadow: 'none',
-      backgroundColor: '#0058c4',
-      borderColor: '#005cbf',
-    },
-    '&:focus': {
-      boxShadow: '0 0 0 0.2rem rgba(0,123,255,.5)',
-    },
-  },
-})(Button);
+import { makeStyles} from '@material-ui/core/styles';
+import StyleButton from './index.styles';
 
 const useStyles = makeStyles((theme) => ({
   margin: {
@@ -47,9 +8,8 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-const CustomizedButton = ({ value }) => {
+const Button = ({ value }) => {
   const classes = useStyles();
-
   return (
     <div>
       <StyleButton variant="contained" color="primary" className={classes.margin}>
@@ -59,4 +19,4 @@ const CustomizedButton = ({ value }) => {
   );
 }
 
-export default CustomizedButton;
+export default Button;
